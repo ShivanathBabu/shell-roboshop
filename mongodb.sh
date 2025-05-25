4d7244e696a26ddd27c9ee194aa6671f60e3c7fa
shivanath <shivanathbabu.kodimyala@gmail.com>
2025-05-25 11:11:50 +0530

    roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -5,10 +5,10 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-logs_folder=="/var/log/roboshop-logs"
+logs_folder="/var/log/roboshop-logs"
 script_name=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$logs_folder/$script_name.log"
-mkdir -p $LOG_FOLDER
+mkdir -p $logs_folder
 
 if [ $userid -ne 0 ]
 then
