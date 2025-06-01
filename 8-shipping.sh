9df31be31f27824230680474af3a401eb4bcc597
shivanath <shivanathbabu.kodimyala@gmail.com>
2025-06-01 14:38:10 +0530

    roboshop

--- 8-shipping.sh
+++ 8-shipping.sh
@@ -67,10 +67,10 @@ systemctl daemon-reload &>>$script_file
 validate $? "reload shipping"
 
 systemctl enable shipping &>>$script_file
-validate $? "enable shipping.service"
+validate $? "enable shipping service"
 
 systemctl start shipping &>>$script_file
-validate $? "starting shipping.service"
+validate $? "starting shipping service"
 
 dnf install mysql -y &>>$script_file
 validate $? "installing sql"
